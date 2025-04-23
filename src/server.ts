cb412fd673a9c44308cbd23f6104947435b364dd
Brandon Korous <me@brandonkorous.com>
2025-04-22 21:49:04 -0700

    fix: specify host in app.listen for better accessibility

--- src/server.ts
+++ src/server.ts
@@ -35,7 +35,7 @@ app.setNotFoundHandler((request, reply) => {
     reply.status(404).send({ error: 'Not Found' });
 });
 
-app.listen({ port: port }, (err, address) => {
+app.listen({ port: port, host: '0.0.0.0' }, (err, address) => {
     if (err) {
         Sentry.captureException(err);
         console.error(err);
